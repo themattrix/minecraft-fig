2c00e466de002a0b56601e4286629a3024dcff45
Matthew Tardiff <mattrix@gmail.com>
2015-03-12 17:27:35 -0500

    Better EULA handling

--- containers/game/run.sh
+++ containers/game/run.sh
@@ -11,15 +11,11 @@ set -e
 set -o pipefail
 
 function minecraft() {
-    java -Xmx1024M -Xms1024M -jar minecraft.jar nogui 2>&1 | tee server.out
+    java -Xmx1024M -Xms1024M -jar minecraft.jar nogui
 }
 
 # Link all settings into the Minecraft folder
 ln -sf settings/* .
 
+echo "eula=true" > eula.txt
 minecraft
-
-if grep -sq "You need to agree to the EULA" server.out; then
-    sed -i -s -e 's/eula=false/eula=true/' eula.txt
-    minecraft
-fi
