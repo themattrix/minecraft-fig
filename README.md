56704751887961649f50e478fd9b569882639d08
Matthew Tardiff <mattrix@gmail.com>
2015-03-25 13:38:22 -0500

    The Spigot JAR is now preserved between runs. Custom server settings now override the defaults.

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ Usage:
     $ docker-compose up -d map game   # once
     # docker-compose up -d mapcrafter # periodically
 
-> Run the `mapcrafter` container periodically (perhaps with a cron job) to ensure that the map is kept up to date.
+> Run the `mapcrafter` container periodically (perhaps in a loop or with a cron job) to ensure that the map is kept up to date.
 
-The Minecraft server is hosted on port 25565, and the map web server on 25566.
+The Minecraft server is hosted on port 25565, and the map web server on 80, although these can be changed in `docker-compose.yml`.
+
+
+## Custom Server Settings
+
+The default settings are stored in `volumes/game/settings-default/`. You can either change these directly, or override any of these
+settings files in `volumes/game/settings-custom/`.
+
+> I like to keep my custom settings in a private repo, which I can then clone to `settings-custom`.
--- containers/game/run.sh
+++ containers/game/run.sh
@@ -6,22 +6,77 @@
 # Assumption: current working directory is /minecraft
 #
 
-# Exit if any line fails
 set -e
 set -o pipefail
 
-# If the white-list.{txt,json} file exists and is non-empty, enable white-listing.
-if [[ -s "white-list.txt" ]] || [[ -s "whitelist.json" ]]; then
-    sed -i -s -e '/^white-list=/{s/=false/=true/}' "server.properties"
-fi
+readonly SPIGOT_BUILDTOOLS_URL="https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
+readonly SPIGOT_SRC="/minecraft/volumes/spigot/spigot.jar"
+readonly SPIGOT_DST="/minecraft/spigot.jar"
 
-# Accept the EULA so the server runs.
-echo "eula=true" > eula.txt
+function in_temp_dir() {
+    local temp_dir=$(mktemp -d)
 
-# Start the Minecraft server.
-mark2 start
+    function cleanup() {
+        cd && rm -rf "${temp_dir}"
+    }
 
-# Block while the server is running.
-while ps ax | grep -sqF mark2; do
-    sleep 5
-done
+    (trap cleanup EXIT; cd "${temp_dir}"; "$@")
+}
+
+function spigot_exists() {
+    [ -f "${SPIGOT_SRC}" ]
+}
+
+function build_spigot() {
+    wget "${SPIGOT_BUILDTOOLS_URL}"
+    HOME=$PWD java -jar BuildTools.jar
+    mv Spigot/Spigot-Server/target/spigot-*.jar "${SPIGOT_SRC}"
+}
+
+function ensure_spigot() {
+    if ! spigot_exists; then
+        in_temp_dir build_spigot
+    fi
+
+    cp "${SPIGOT_SRC}" "${SPIGOT_DST}"
+}
+
+function main() {
+    # Create expected volume subdirectories
+    mkdir -p volumes/{world,settings-default,settings-custom,spigot}
+
+    # Copy default settings
+    if ls volumes/settings-default/* &> /dev/null; then
+        cp -f volumes/settings-default/* .
+    fi
+
+    # Symlink custom settings, stomping defaults
+    if ls volumes/settings-custom/* &> /dev/null; then
+        ln -sf volumes/settings-custom/* .
+    fi
+
+    # Copy the mark2.properties files to the expected location.
+    mkdir -p /etc/mark2 && cp -L mark2.properties /etc/mark2/mark2.properties
+
+    # Make the world data available to Minecraft
+    ln -s volumes/world world
+
+    # Accept the EULA so that the server runs.
+    echo "eula=true" > eula.txt
+
+    # Build and/or copy spigot to /minecraft/spigot.jar
+    ensure_spigot
+
+    # Start the Minecraft server.
+    mark2 start
+
+    # Block while Mark2 is running.
+    while ps ax | grep -sqF mark2; do
+        sleep 1
+    done
+
+    # Attempt to stop the server *just in case* it still happens to be running.
+    mark2 stop &> /dev/null || true
+}
+
+main "$@"
